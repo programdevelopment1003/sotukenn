5c15687bd4d1fe82f53972055430a1de78b8828c
深町英輝 <programdevelopment1003@gmail.com>
2018-10-10 14:20:44 +0900

    no message

--- src/main/java/com/example/demo/Loin.java
+++ src/main/java/com/example/demo/Loin.java
@@ -1,8 +1,11 @@
 package com.example.demo;
 
 import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+
 
 @Controller
 public class Loin {
